369d1c802a66eb7777c243482c19591d27a065c9
edveloso <edveloso@gmail.com>
2019-10-11 08:22:42 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-qpp';
+
+  pessoas = [];
+  //pessoas: Object[] = [
+   // {"id": 1,
+   //  "nome": "veloso"},
+   //  {"id": 2,
+   //  "nome": "moura"}     
+  //];
+
+  constructor(http: HttpClient){
+    http.get("http://localhost:8080/pessoa")
+        .subscribe(
+          data => {
+            console.log(data);
+            this.pessoas = data;
+          }
+        );
+
+  }
+
 }
